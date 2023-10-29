ca42b72c13deb1a61ffcdf6bcc00cd022667316b
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-29 14:40:11 +0530

    Add body-parser, routes, and CORS to index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import router from './routes/index.js';
 const app = express();
 
-const PORT = process.env.PORT || '8000';
 
-app.get('/', (req, res) => {
-	res.send('Team AAAG');
-})
+app.use(bodyParser.json({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use('/',router);
+
+const PORT = process.env.PORT || '8000';
 
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`);
