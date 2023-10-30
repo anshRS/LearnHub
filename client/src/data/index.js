32ac1cab588d112511449ad89e9ebdda54ce2d3a
anshRS <anshraj0426@gmail.com>
2023-10-30 11:22:35 +0530

    Created file for dummy data

---
+++ client/src/data/index.js
@@ -0,0 +1,34 @@
+const Fields = [
+    {
+        icon: '🔧',
+        title: 'Engineering',
+        description: 'Explore the world of engineering and innovation.',
+    },
+    {
+        icon: '⚕️',
+        title: 'Medical',
+        description: 'Discover a career in the medical and healthcare industry.',
+    },
+    {
+        icon: '💻',
+        title: 'Development',
+        description: 'Learn software development and programming skills.',
+    },
+    {
+        icon: '🎨',
+        title: 'Designer',
+        description: 'Unleash your creativity as a designer.',
+    },
+    {
+        icon: '📈',
+        title: 'Marketing',
+        description: 'Promote products and brands through marketing strategies.',
+    },
+    {
+        icon: '🧪',
+        title: 'Science',
+        description: 'Dive into the fascinating world of science and research.',
+    },
+];
+
+export { Fields };
