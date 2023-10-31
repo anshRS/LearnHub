98dc1290350a3068918a2363ee7f705e2da26c73
anshRS <anshraj0426@gmail.com>
2023-10-31 16:52:28 +0530

    Added routes & logout feature in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
 "use client"
 import React, { useState } from 'react'
 import { Article, Books, Buildings, House, Video } from "@phosphor-icons/react";
+import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import { signOut } from '@/redux/slices/auth';
+import { toast } from 'react-toastify';
+
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isProgramsOpen, setIsProgramsOpen] = useState(false);
+    const { isLoggedIn } = useSelector((state) => state.auth)
+    const dispatch = useDispatch();
 
     const toggleProgramsDropdown = () => {
         setIsProgramsOpen(!isProgramsOpen);
+    };
+
+    const logout = () => {
+        dispatch(signOut())
+        
+        toast.success("Logout Successful!", {
+            position: "top-center",
+        });
     };
 
     return (
@@ -29,60 +44,77 @@ const Navbar = () => {
                 <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
                     <li className="flex gap-2 justify-center items-center">
                         <House size={20} />
-                        <a className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a>
+                        <Link className="text-sm text-gray-400 hover:text-gray-500" href="/">Home</Link>
                     </li>
 
                     <li className="relative group flex gap-2 justify-center items-center">
-                    <Video size={20} />
-                    <a
-                        className="text-sm text-gray-400 hover:text-gray-500 cursor-pointer"
-                        onClick={toggleProgramsDropdown}
-                    >
-                        Programs
-                        <svg
-                            className={`ml-2 w-4 h-4 inline-block transition-transform transform ${
-                                isProgramsOpen ? 'rotate-180' : ''
-                            }`}
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
+                        <Video size={20} />
+                        <a
+                            className="text-sm text-gray-400 hover:text-gray-500 cursor-pointer"
+                            onClick={toggleProgramsDropdown}
                         >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M19 9l-7 7-7-7"
-                            ></path>
-                        </svg>
-                    </a>
-                    {isProgramsOpen && (
-                        <div className="absolute top-5 left-0 mt-2 space-y-2 bg-white text-gray-600 border rounded">
-                            <a className="block px-4 py-1 hover:bg-gray-100" href="#">
-                                Engineering
-                            </a>
-                            <a className="block px-4 py-1 hover:bg-gray-100" href="#">
-                                Medical
-                            </a>
-                            <a className="block px-4 py-1 hover:bg-gray-100" href="#">
-                                Technology
-                            </a>
-                        </div>
-                    )}
-                </li>
+                            Programs
+                            <svg
+                                className={`ml-2 w-4 h-4 inline-block transition-transform transform ${isProgramsOpen ? 'rotate-180' : ''
+                                    }`}
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M19 9l-7 7-7-7"
+                                ></path>
+                            </svg>
+                        </a>
+                        {isProgramsOpen && (
+                            <div className="absolute top-5 left-0 mt-2 space-y-2 bg-white text-gray-600 border rounded">
+                                <a className="block px-4 py-1 hover:bg-gray-100" href="#">
+                                    Engineering
+                                </a>
+                                <a className="block px-4 py-1 hover:bg-gray-100" href="#">
+                                    Medical
+                                </a>
+                                <a className="block px-4 py-1 hover:bg-gray-100" href="#">
+                                    Technology
+                                </a>
+                            </div>
+                        )}
+                    </li>
 
                     <li className="flex gap-2 justify-center items-center">
                         <Article size={20} />
-                        <a className="text-sm text-gray-400 hover:text-gray-500" href="#">Blogs</a>
+                        <Link className="text-sm text-gray-400 hover:text-gray-500" href="/blogs">Blogs</Link>
                     </li>
 
                     <li className="flex gap-2 justify-center items-center">
                         <Buildings size={20} />
-                        <a className="text-sm text-gray-400 hover:text-gray-500" href="#">About</a>
+                        <Link className="text-sm text-gray-400 hover:text-gray-500" href="/about">About</Link>
                     </li>
                 </ul>
-                <a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Log In</a>
-                <a className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a>
+                {
+                    isLoggedIn && (
+                        <>                            
+                            <button className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" onClick={() => {
+                                logout();
+                            }}>Logout</button>
+                        </>
+                    )
+                }
+
+                {
+                    !isLoggedIn && (
+                        <>
+                            <Link className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="/auth/login">Log In</Link>
+                            <Link className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="/auth/signup">Sign up</Link>
+                        </>
+                    )
+                }
+
+
             </nav>
         </>
     );
