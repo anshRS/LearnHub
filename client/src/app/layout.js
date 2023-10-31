2bf665ed5cc00b706afc9f1f33dcb135e1b95b12
anshRS <anshraj0426@gmail.com>
2023-10-31 16:47:48 +0530

    Setup redux provider to root layout

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,6 +2,9 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import "react-toastify/dist/ReactToastify.css";
+import { ReduxProvider } from '@/redux/provider'
+import { ToastContainer } from 'react-toastify';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +17,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer/>
+        <ReduxProvider>
+          <ToastContainer/>
+          <Navbar />
+          {children}
+          <Footer />
+        </ReduxProvider>
       </body>
     </html>
   )
