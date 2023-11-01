a57da71406787378adb7d4308769a5321632e9fb
anshRS <anshraj0426@gmail.com>
2023-11-02 02:39:02 +0530

    Updated Post blog api based on new Model

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -1,94 +1,99 @@
-import  BlogModel from "../models/blog.js";
+import BlogModel from "../models/blog.js";
 
 
-export const searchBlogs = async (req, res)=>{
+export const searchBlogs = async (req, res) => {
     try {
-        const {title , category} = req.body ;
+        const { text, field } = req.body;
         //  Search Logic
 
-        title = title.toLowerCase();
-        const title_query = {title : title}
-        const field_query = {category : category}
+        text = text.toLowerCase();
+        const title_query = { title: text }
+        const field_query = { field: field }
         const title_blogs = BlogModel.find(title_query).toArray();
         const field_blogs = BlogModel.find(field_query).toArray();
-        if(!title_blogs){
-            if(field_blogs){
-                res.status(201).json({message:"No blogs with given title" , blogs : field_blogs})
+        if (!title_blogs) {
+            if (field_blogs) {
+                res.status(201).json({ message: "Not blogs with given title", blogs: field_blogs })
             }
-            else{
-                return res.status(200).json({mesage : "Not Found"});
+            else {
+                return res.status(200).json({ mesage: "Not Found" });
             }
         }
-        res.status(200).json({blogs : title_blogs });
+        res.status(200).json({ blogs: title_blogs });
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
 
 };
 
-export const postBlog = async (req ,res,next)=>{
-    try{
-        const {title , content , author, category} = req.body ;
+export const postBlog = async (req, res) => {
+    console.log(req.body)
+    console.log(req.file)
+    try {
+        const { title, subtitle, descriptions, author, discipline } = req.body;
         const image = req.file;
-        console.log({title , content , author, category,image})
-        if(!title || !content || !author || !category || !image){
-            return res.status(400).send({msg:"Please provide all fields"})
+        
+        if (!title || !subtitle || !descriptions || !author || !image || !discipline) {
+            return res.status(400).send({ msg: "Please provide all fields" })
         }
+
         const newBlog = new BlogModel({
-            title ,
-            content ,
-            author , 
-            category,
+            title,
+            subtitle,
+            descriptions,
+            author,
+            discipline,
             imageUrl: image.filename,
         });
         await newBlog.save();
-        res.status(200).json({message : "Successfully Saved !!"});
+        res.status(200).json({ message: "Successfully Saved !!" });
     }
     catch (err) {
+        console.log(err)
         res.status(500).json({ error: err.message })
     }
 };
 
-export const getBlog = async (req , res)=>{
-    try{
-        const blogid =  req.params.id;
+export const getBlog = async (req, res) => {
+    try {
+        const blogid = req.params.id;
 
-        const blog =await BlogModel.findById(blogid);
-        if(!blog){
-            res.status(400).json({message : "Blog not found"});
+        const blog = await BlogModel.findById(blogid);
+        if (!blog) {
+            res.status(400).json({ message: "Blog not found" });
         }
-        res.status(200).json({blog})
+        res.status(200).json({ blog })
     }
-    catch(error){
-        res.status(500).json({error : err.message})
+    catch (error) {
+        res.status(500).json({ error: err.message })
     }
 }
 
-export const getAllBlogs = async (req , res)=>{
-    try{
-        const blogs = await BlogModel.find();        
+export const getAllBlogs = async (req, res) => {
+    try {
+        const blogs = await BlogModel.find();
 
         res.status(200).json({
-            msg:"Blogs fetched successfully",
+            msg: "Blogs fetched successfully",
             blogs
         })
 
     }
-    catch(error){
-        res.status(500).json({error : err.message})
+    catch (error) {
+        res.status(500).json({ error: err.message })
     }
 }
 
-export const getBlogsByCategory = async (req,res) => {
-    try{
+export const getBlogsByCategory = async (req, res) => {
+    try {
         const blogCategory = req.params.category;
-        const blogs = await BlogModel.find({category: blogCategory});
+        const blogs = await BlogModel.find({ category: blogCategory });
         res.status(200).json({
-            msg:"Blogs fetched successfully",
+            msg: "Blogs fetched successfully",
             blogs
         })
-    }catch(err){
-        res.status(500).json({error:err})
+    } catch (err) {
+        res.status(500).json({ error: err })
     }
 }
 
