4d7888a0f2310cc18f127bcf81ad63f6ee28e1d4
anshRS <anshraj0426@gmail.com>
2023-11-01 20:42:41 +0530

    Added active Navbar link styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
 import { signOut } from '@/redux/slices/auth';
 import { toast } from 'react-toastify';
+import { usePathname } from 'next/navigation';
 
 
 const Navbar = () => {
@@ -12,6 +13,7 @@ const Navbar = () => {
     const [isProgramsOpen, setIsProgramsOpen] = useState(false);
     const { isLoggedIn } = useSelector((state) => state.auth)
     const dispatch = useDispatch();
+    const currentRoute = usePathname();
 
     const toggleProgramsDropdown = () => {
         setIsProgramsOpen(!isProgramsOpen);
@@ -19,7 +21,7 @@ const Navbar = () => {
 
     const logout = () => {
         dispatch(signOut())
-        
+
         toast.success("Logout Successful!", {
             position: "top-center",
         });
@@ -29,10 +31,10 @@ const Navbar = () => {
         <>
             <nav className="fixed top-0 left-0 right-0 z-50 px-4 py-4 flex justify-between items-center bg-white border-y">
 
-                <a className="flex text-xl font-bold leading-none items-center gap-3" href="#">
+                <Link className="flex text-xl font-bold leading-none items-center gap-3" href="/">
                     <Books size={32} />
                     LearnHub
-                </a>
+                </Link>
                 <div className="lg:hidden">
                     <button className="navbar-burger flex items-center text-blue-600 p-3">
                         <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -42,10 +44,21 @@ const Navbar = () => {
                     </button>
                 </div>
                 <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
-                    <li className="flex gap-2 justify-center items-center">
-                        <House size={20} />
-                        <Link className="text-sm text-gray-400 hover:text-gray-500" href="/">Home</Link>
-                    </li>
+                    {
+                        currentRoute === '/' ? (
+                            <li className="flex gap-2 justify-center items-center bg-blue-500 text-white rounded-md p-2 hover:text-gray-100">
+                                <House size={20} />
+                                <Link className="text-sm"
+                                    href="/">Home</Link>
+                            </li>
+                        ) : (
+                            <li className="flex gap-2 justify-center items-center">
+                                <House size={20} />
+                                <Link className="text-sm text-gray-400 hover:text-gray-500"
+                                    href="/">Home</Link>
+                            </li>
+                        )
+                    }                    
 
                     <li className="relative group flex gap-2 justify-center items-center">
                         <Video size={20} />
@@ -72,32 +85,55 @@ const Navbar = () => {
                         </a>
                         {isProgramsOpen && (
                             <div className="absolute top-5 left-0 mt-2 space-y-2 bg-white text-gray-600 border rounded">
-                                <a className="block px-4 py-1 hover:bg-gray-100" href="#">
+                                <Link className="block px-4 py-1 hover:bg-gray-100" href="/programs/engineering">
                                     Engineering
-                                </a>
-                                <a className="block px-4 py-1 hover:bg-gray-100" href="#">
+                                </Link>
+                                <Link className="block px-4 py-1 hover:bg-gray-100" href="/programs/medical">
                                     Medical
-                                </a>
-                                <a className="block px-4 py-1 hover:bg-gray-100" href="#">
+                                </Link>
+                                <Link className="block px-4 py-1 hover:bg-gray-100" href="/programs/technology">
                                     Technology
-                                </a>
+                                </Link>
                             </div>
                         )}
                     </li>
 
-                    <li className="flex gap-2 justify-center items-center">
-                        <Article size={20} />
-                        <Link className="text-sm text-gray-400 hover:text-gray-500" href="/blogs">Blogs</Link>
-                    </li>
+                    {
+                        currentRoute === '/blogs' ? (
+                            <li className="flex gap-2 justify-center items-center bg-blue-500 text-white rounded-md p-2 hover:text-gray-100">
+                                <Article size={20} />
+                                <Link className="text-sm"
+                                    href="/blogs">Blogs</Link>
+                            </li>
+                        ) : (
+                            <li className="flex gap-2 justify-center items-center">
+                                <Article size={20} />
+                                <Link className="text-sm text-gray-400 hover:text-gray-500"
+                                    href="/blogs">Blogs</Link>
+                            </li>
+                        )
+                    }     
 
-                    <li className="flex gap-2 justify-center items-center">
-                        <Buildings size={20} />
-                        <Link className="text-sm text-gray-400 hover:text-gray-500" href="/about">About</Link>
-                    </li>
+                    {
+                        currentRoute === '/about' ? (
+                            <li className="flex gap-2 justify-center items-center bg-blue-500 text-white rounded-md p-2 hover:text-gray-100">
+                                <Buildings size={20} />
+                                <Link className="text-sm"
+                                    href="/about">About</Link>
+                            </li>
+                        ) : (
+                            <li className="flex gap-2 justify-center items-center">
+                                <Buildings size={20} />
+                                <Link className="text-sm text-gray-400 hover:text-gray-500"
+                                    href="/about">About</Link>
+                            </li>
+                        )
+                    } 
+                    
                 </ul>
                 {
                     isLoggedIn && (
-                        <>                            
+                        <>
                             <button className="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" onClick={() => {
                                 logout();
                             }}>Logout</button>
