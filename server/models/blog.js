18da0f4ff020d43683de4a54d2ff3777620b3c47
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-01 18:51:58 +0530

    Updated blog model

--- server/models/blog.js
+++ server/models/blog.js
@@ -1,10 +1,31 @@
 import mongoose from "mongoose";
-const blogSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    imageUrl: { type: String, required: true },
-  },{ timestamps: true });
-  
-const Blog = mongoose.model('BlogModel', blogSchema);
-export default Blog; 
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: { 
+      type: String, 
+      required: true 
+    },
+    content: { 
+      type: String,
+      required: true
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    category:{
+      type: String,
+      required:true,
+    },
+    imageUrl: { 
+      type: String,
+      required: true 
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model("BlogModel", blogSchema);
+export default Blog;
