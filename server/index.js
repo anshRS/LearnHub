c224a8128f8099d0019011c5539387dd4f753816
anshRS <anshraj0426@gmail.com>
2023-11-02 21:06:47 +0530

    Developed GET endpoints for Courses

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import {Authrouter} from './routes/auth.js';
 import {Blogrouter} from './routes/blog.js';
+import { CourseRouter } from './routes/course.js';
 import ConnectDB from './database/db.js';
 import {ProfileRouter} from "./routes/dashboard.js"
 import path from 'path'
@@ -25,6 +26,7 @@ app.use(express.static(path.join(process.cwd(), "uploads")));
 app.use('/api/auth' , Authrouter);
 app.use("/api/blog" , Blogrouter);
 app.use("/api/dashboard"  , ProfileRouter);
+app.use("/api/courses" , CourseRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`);
