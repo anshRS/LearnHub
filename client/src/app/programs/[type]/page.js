d1d9e107fbd0c8ddb2d7ebeef5a8ef26781b7507
anshRS <anshraj0426@gmail.com>
2023-11-02 21:09:42 +0530

    Integrated GET all courses api

--- client/src/app/programs/[type]/page.js
+++ client/src/app/programs/[type]/page.js
@@ -1,18 +1,32 @@
+"use client"
+
 import Program from "@/components/Program"
-import { CourseData } from "@/data";
+import axios from "@/utils/axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function ProgramPage({ params }) {
     const type = params.type;
-    const courses = CourseData.filter(item => item.discipline.toLowerCase() === type.toLowerCase());
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+        const getAllCourses = async () => {
+            const res = await axios.get(`/api/courses/c/${type}`)           
+            setCourses(res.data)
+        }
+        getAllCourses()
+    }, [])
 
     return (
-        <div className="mt-32">
+        <div className="mt-32 h-screen">
             <div className="bg-white mt-32">
                 <div className="w-3/4 grid md:grid-cols-2 gap-5 mx-auto rounded-md">
                     {
-                        courses.map((course, i) => {                            
+                        courses.map((course, i) => {
                             return (
-                                <Program key={i} name={course.name} discipline={course.discipline} metadata={course.metadata.professor} />
+                                <Link key={i} href={`/programs/${type}/${course._id}`}>
+                                    <Program name={course.name} discipline={course.discipline} metadata={course.metadata.professor} />
+                                </Link>
                             )
                         })
                     }
